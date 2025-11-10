7df5b0f0ea72efbd6a361d175d65ef625737d3b5
asmiwas <as@miwas.no>
2025-11-11 00:34:57 +0100

    Also changed the supabase vard in redditUtils.js

--- components/EzoicScripts.jsx
+++ components/EzoicScripts.jsx
@@ -1,38 +1,3 @@
-// "use client";
-// import Script from "next/script";
-
-// export default function EzoicScripts() {
-//   return (
-//     <>
-//       {/* 🧩 Ezoic Privacy Scripts – må lastes først */}
-//       <Script
-//         src="https://cmp.gatekeeperconsent.com/min.js"
-//         data-cfasync="false"
-//         strategy="beforeInteractive"
-//       />
-//       <Script
-//         src="https://the.gatekeeperconsent.com/cmp.min.js"
-//         data-cfasync="false"
-//         strategy="beforeInteractive"
-//       />
-
-//       {/* 🧩 Hoved Header Script */}
-//       <Script
-//         async
-//         src="//www.ezojs.com/ezoic/sa.min.js"
-//         strategy="beforeInteractive"
-//       />
-
-//       {/* 🧩 Init Script */}
-//       <Script id="ez-init" strategy="beforeInteractive">
-//         {`
-//           window.ezstandalone = window.ezstandalone || {};
-//           ezstandalone.cmd = ezstandalone.cmd || [];
-//         `}
-//       </Script>
-//     </>
-//   );
-// }
 "use client";
 import Script from "next/script";
 
